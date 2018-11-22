fd67b8fd3e50db99ef6da4c40ea98e9b01116642
JoeEmp <2057567309@qq.com>
2018-11-22 21:16:15 +0800

    Update python/excel.py

--- python/excel.py
+++ python/excel.py
@@ -5,7 +5,7 @@ import re
 methods={"equal":1,"unequal":2,"contain":3,"exclusive":4}
 rank={"row":1,"col":2}
 
-#返回字典 格式为{'title':[]}
+#返回字典 index=0 格式为[{'title':[]}] index！=0 返回某一个表
 def readExcel(filename,index=0):
     try:
         f=open(filename,'rb')
@@ -22,6 +22,11 @@ def readExcel(filename,index=0):
         return tableList[index-1]
     return tableList
 
+def readRecord(table,index=0):
+    record=[]
+    for key in table:
+        record.append(table[key][index])
+    return record
 
 if __name__ == '__main__':
     ifn = r"in.txt"
