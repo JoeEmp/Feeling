6083ede27c2dc7eccc711ef2b60601054c299f2b
JoeEmp <2057567309@qq.com>
2018-11-22 21:08:27 +0800

    Update python/excel.py

--- python/excel.py
+++ python/excel.py
@@ -6,35 +6,22 @@ methods={"equal":1,"unequal":2,"contain":3,"exclusive":4}
 rank={"row":1,"col":2}
 
 #返回字典 格式为{'title':[]}
-def readExcel(filename,sheets=0):
-    # 打开文件
-    excelDict={}
-    exc=xlrd.open_workbook(filename)
-    table=exc.sheets()[sheets]
-    length=len(table.row_values(0))
-    for i in range(length):
-        excelDict[table.row_values(0)[i]]=table.col_values(i)[1:]
-    return excelDict
+def readExcel(filename,index=0):
+    try:
+        f=open(filename,'rb')
+        book=xlrd.open_workbook(f)
+    except Exception as e:
+        print(e)
+    tableList=[]
+    for sheet in book.sheets():
+        table={}
+        for j in range(sheet.ncols):
+            table[sheet.cell(0,j).value]=sheet.col_values(j)[1:]
+        tableList.append(table)
+    if index!=0:
+        return tableList[index-1]
+    return tableList
 
-#统计某行数据和
-def sumCol(dict,title):
-    length=len(dict[title])
-    sum=0
-    for i in range(length):
-        sum+=float(dict[title][i])
-    return sum
-
-'''
-按条件筛选行
-'''
-def sieveRow(dict,title,method="equal",values=0):
-    indexList=[]
-
-    return dict
-
-def assertBook(sheetName):
-
-    pass
 
 if __name__ == '__main__':
     ifn = r"in.txt"
